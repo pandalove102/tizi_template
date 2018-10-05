c525ad512d83acf924994f1dc073919aaae4590f
Pandalove102 <phamminhphuc102@gmail.com>
2018-10-05 13:44:35 +0700

    update trưa 5/10

--- view/404.php
+++ view/404.php
@@ -1,40 +1,187 @@
-
-
-<div class="page-header" style="background: url(assets/img/banner1.jpg);">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="breadcrumb-wrapper">
-					<h2 class="product-title">404</h2>
-					<ol class="breadcrumb">
-						<li><a href="#">Home /</a></li>
-						<li class="current">404</li>
-					</ol>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="row-1">
+            <div class="gruop-category">
+    <div class="box-title">
+        <div class="title title-category">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+            <h2>Danh mục sản phẩm</h2>
+        </div>
+    </div>
+    <div id="slide-category-home" class="category-index  owl-carousel owl-theme owl-loaded owl-drag">
+        
+    <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 1520px;"><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                                                    <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/nha-dat-c45.html" title="Nhà Đất" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/thiet-ke-1512446741.gif" alt="Nhà Đất">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/nha-dat-c45.html" title="Nhà Đất">
+                                Nhà Đất                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/xe-co-phu-tung-c58.html" title="Xe Cộ, Phụ Tùng" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/xe-co-1512447100.jpg" alt="Xe Cộ, Phụ Tùng">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/xe-co-phu-tung-c58.html" title="Xe Cộ, Phụ Tùng">
+                                Xe Cộ, Phụ Tùng                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/dien-tu-dien-may-gia-dung-c76.html" title="Điện Tử, Điện Máy, Gia Dụng" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/do-dien-tu-cho-icon-1512457578.png" alt="Điện Tử, Điện Máy, Gia Dụng">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/dien-tu-dien-may-gia-dung-c76.html" title="Điện Tử, Điện Máy, Gia Dụng">
+                                Điện Tử, Điện Máy, Gia Dụng                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/thuc-pham-dia-diem-an-uong-c93.html" title="Thực Phẩm, Địa Điểm Ăn Uống" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/thuc-pham-icon1-1512458689.jpg" alt="Thực Phẩm, Địa Điểm Ăn Uống">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/thuc-pham-dia-diem-an-uong-c93.html" title="Thực Phẩm, Địa Điểm Ăn Uống">
+                                Thực Phẩm, Địa Điểm Ăn Uống                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/thoi-trang-my-pham-c102.html" title="Thời Trang, Mỹ Phẩm" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/icon-thoi-trang-1512548432.png" alt="Thời Trang, Mỹ Phẩm">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/thoi-trang-my-pham-c102.html" title="Thời Trang, Mỹ Phẩm">
+                                Thời Trang, Mỹ Phẩm                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/me-va-be-c114.html" title="Mẹ Và Bé" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/me-va-be-1512549379.png" alt="Mẹ Và Bé">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/me-va-be-c114.html" title="Mẹ Và Bé">
+                                Mẹ Và Bé                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/noi-that---ngoai-that-c121.html" title="Nội Thất - Ngoại Thất" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/icon-noi-that-1512549900.png" alt="Nội Thất - Ngoại Thất">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/noi-that---ngoai-that-c121.html" title="Nội Thất - Ngoại Thất">
+                                Nội Thất - Ngoại Thất                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/lam-dep-suc-khoe-the-thao-c130.html" title="Làm Đẹp, Sức Khỏe, Thể Thao" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/icon-suc-khoe-1512550428.png" alt="Làm Đẹp, Sức Khỏe, Thể Thao">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/lam-dep-suc-khoe-the-thao-c130.html" title="Làm Đẹp, Sức Khỏe, Thể Thao">
+                                Làm Đẹp, Sức Khỏe, Thể Thao                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/cong-nghiep-nong-nghiep-xay-dung-c137.html" title="Công Nghiệp, Nông Nghiệp, Xây Dựng" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/cong-nghiep-1512551317.png" alt="Công Nghiệp, Nông Nghiệp, Xây Dựng">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/cong-nghiep-nong-nghiep-xay-dung-c137.html" title="Công Nghiệp, Nông Nghiệp, Xây Dựng">
+                                Công Nghiệp, Nông Nghiệp, Xây Dựng                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/do-dung-sinh-hoat-giai-tri-so-thich-c143.html" title="Đồ Dùng Sinh Hoạt, Giải Trí, Sở Thích" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/dogiadung-icon0-1512552247.png" alt="Đồ Dùng Sinh Hoạt, Giải Trí, Sở Thích">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/do-dung-sinh-hoat-giai-tri-so-thich-c143.html" title="Đồ Dùng Sinh Hoạt, Giải Trí, Sở Thích">
+                                Đồ Dùng Sinh Hoạt, Giải Trí, Sở Thích                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item active" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/viec-lam---tuyen-sinh-c144.html" title="Việc Làm - Tuyển Sinh" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/viec-lam-1512552440.png" alt="Việc Làm - Tuyển Sinh">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/viec-lam---tuyen-sinh-c144.html" title="Việc Làm - Tuyển Sinh">
+                                Việc Làm - Tuyển Sinh                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/du-lich-c169.html" title="Du Lịch" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/unnamed-1512554909.png" alt="Du Lịch">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/du-lich-c169.html" title="Du Lịch">
+                                Du Lịch                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/hop-tac-ket-ban-c178.html" title="Hợp Tác, Kết Bạn" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/hop-tac-1512555578.jpg" alt="Hợp Tác, Kết Bạn">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/hop-tac-ket-ban-c178.html" title="Hợp Tác, Kết Bạn">
+                                Hợp Tác, Kết Bạn                            </a>
+                        </div>
+                    </div>   
+                                                        <div class="box-item last">
+                        <div class="img">
+                            <a class="width-x" href="/dich-vu-c182.html" title="Dịch Vụ" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/dich-vuj-1512555449.png" alt="Dịch Vụ">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/dich-vu-c182.html" title="Dịch Vụ">
+                                Dịch Vụ                            </a>
+                        </div>
+                    </div>   
+                                                            </div></div><div class="owl-item" style="width: 190px;"><div class="item-category">
+                                        <div class="box-item fisrt">
+                        <div class="img">
+                            <a class="width-x" href="/van-phong-pham---qua-tang-c197.html" title="Văn Phòng Phẩm - Quà Tặng" style="height: 118.3px;">
+                                <img src="http://static.muabanvn.net/media/images/category/ico/images-1513908040.png" alt="Văn Phòng Phẩm - Quà Tặng">
+                            </a>
+                        </div>
+                        <div class="content">
+                            <a href="/van-phong-pham---qua-tang-c197.html" title="Văn Phòng Phẩm - Quà Tặng">
+                                Văn Phòng Phẩm - Quà Tặng                            </a>
+                        </div>
+                    </div>   
+                                    </div></div></div></div><div class="owl-nav"><div class="owl-prev disabled"><i class="fa fa-angle-left" aria-hidden="true"></i></div><div class="owl-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div></div><div class="owl-dots"><div class="owl-dot active"><span></span></div><div class="owl-dot"><span></span></div></div></div>
 </div>
 
-
-<div class="error section-padding">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-				<div class="error-content">
-					<div class="error-message">
-						<h2>404</h2>
-						<h3><span>Ooooops!</span> Something Went Wrong...</h3>
-					</div>
-					<form class="form-error-search">
-						<input type="search" name="search" class="form-control" placeholder="Search Here">
-						<button class="btn btn-common btn-search" type="button">Search Now</button>
-					</form>
-					<div class="description">
-						<span>Or Goto <a href="#">Homepage</a></span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+        </div>
